7893ab378d8af422e652424fbb19887e3c761b89
Thuong Hong <luuthuonghong@gmail.com>
2025-07-11 19:16:33 +0700

    init

---
+++ src/pipeline1/StructuredData.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+def load_csv(file_path):
+    return pd.read_csv(file_path, header=0, sep=',')
+
+
+def clean_numeric(df):
+    pass
+
+def clean_data(df):
+    pass
